8f475308ca94517d838003af8e13d0ca56abf0b2
Johan VandenBerghe <Johan.VandenBerghe@editushome.lu>
2020-02-25 21:53:46 +0100

    xmlfilereader

---
+++ FileReader/FileReader/TextFileReader - Copier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+
+namespace FileReader
+{
+    public class XmlFileReader : IFileReader
+    {
+        public string ReadFile(string path)
+        {
+            string result = string.Empty;
+            XmlTextReader reader = new XmlTextReader(path);
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        result += $"<{reader.Name}>";
+                        break;
+                    case XmlNodeType.Text: //Display the text in each element.
+                        result += reader.Value;
+                        break;
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        result += $"<{reader.Name}>";
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
