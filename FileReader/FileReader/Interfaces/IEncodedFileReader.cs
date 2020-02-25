5525b5a61b815d56cc6a3b3dbd7b4f5b159ae59e
Johan VandenBerghe <Johan.VandenBerghe@editushome.lu>
2020-02-25 22:28:35 +0100

    encoded text file reader

---
+++ FileReader/FileReader/Interfaces/IEncodedFileReader.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FileReader
+{
+    internal interface IEncodedFileReader : IFileReader
+    {
+        string ReadEncodedFile(string path, Func<string, string> decoder);
+    }
+}
--- FileReader/FileReader/TextFileReader.cs
+++ FileReader/FileReader/TextFileReader.cs
@@ -2,11 +2,19 @@
 
 namespace FileReader
 {
-    public class TextFileReader : IFileReader
+    public class TextFileReader : IEncodedFileReader
     {
+        public delegate TResult Decoder<in T1, out TResult>(T1 arg1);
+
         public string ReadFile(string path)
         {
             return System.IO.File.ReadAllText(path);
+        }
+
+        public string ReadEncodedFile(string path, Func<string, string> decoder)
+        {
+            var encodedContend = System.IO.File.ReadAllText(path);
+            return decoder(encodedContend);
         }
     }
 }
