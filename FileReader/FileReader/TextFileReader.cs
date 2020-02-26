e8cc29dad3ecd21a670cf6d8f9a7a0096092a6ff
Johan VandenBerghe <Johan.VandenBerghe@editushome.lu>
2020-02-26 08:58:02 +0100

    perission text file

--- FileReader/FileReader/TextFileReader.cs
+++ FileReader/FileReader/TextFileReader.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.Security;
 
 namespace FileReader
 {
-    public class TextFileReader : IEncodedFileReader
+    public class TextFileReader : SecurisedFileReader, IEncodedFileReader
     {
-        public string ReadFile(string path)
+        public TextFileReader(IPermission permission) : base(permission, new string[] { "toto", "tata" })
         {
+        }
+        public override string ReadFile(string path)
+        {
+            PermissionDemand(path);
             return System.IO.File.ReadAllText(path);
         }
 
         public string ReadEncodedFile(string path, Func<string, string> decoder)
         {
+            PermissionDemand(path);
             var encodedContend = System.IO.File.ReadAllText(path);
             return decoder(encodedContend);
         }
