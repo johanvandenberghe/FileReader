e87720f879d661dc109bc429b768cdae328d938b
Johan VandenBerghe <Johan.VandenBerghe@editushome.lu>
2020-02-27 07:49:07 +0100

    json file role based

--- FileReader/FileReader/JsonFileReader.cs
+++ FileReader/FileReader/JsonFileReader.cs
@@ -5,20 +5,23 @@ using System.Security;
 
 namespace FileReader
 {
-    public class JsonFileReader : IFileReader, IEncodedFileReader
+    public class JsonFileReader : SecurisedFileReader, IEncodedFileReader
     {
+        public JsonFileReader(IPermission permission) : base(permission, new string[] { "toto.json", "tata.json" })
+        {
+        }
+        public override string ReadFile(string path)
+        {
+            PermissionDemand(path);
+            if (path.Contains(".json"))
+                return System.IO.File.ReadAllText(path);
+
+            return  String.Empty;
+        }
         public string ReadEncodedFile(string path, Func<string, string> decoder)
         {
             var encodedJson = ReadFile(path);
             return decoder(encodedJson);
-        }
-
-        public string ReadFile(string path)
-        {
-            if(path.Contains(".json"))
-                return System.IO.File.ReadAllText(path);
-
-            return String.Empty;
         }
     }
 }
