d14da1d315f666c9dd12fe2e0a04681a77c881a6
Johan VandenBerghe <Johan.VandenBerghe@editushome.lu>
2020-02-27 07:40:15 +0100

    add json file reader

---
+++ FileReader/FileReader/JsonFileReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+
+namespace FileReader
+{
+    public class JsonFileReader : IFileReader
+    {
+        public string ReadFile(string path)
+        {
+            if(path.Contains(".json"))
+                return System.IO.File.ReadAllText(path);
+
+            return String.Empty;
+        }
+    }
+}
