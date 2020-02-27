9bd12d39ba029f3d3471b726fe261512519ba4db
Johan VandenBerghe <Johan.VandenBerghe@editushome.lu>
2020-02-27 07:44:43 +0100

    jsonfile encrypted

--- FileReader/FileReader/JsonFileReader.cs
+++ FileReader/FileReader/JsonFileReader.cs
@@ -5,8 +5,14 @@ using System.Security;
 
 namespace FileReader
 {
-    public class JsonFileReader : IFileReader
+    public class JsonFileReader : IFileReader, IEncodedFileReader
     {
+        public string ReadEncodedFile(string path, Func<string, string> decoder)
+        {
+            var encodedJson = ReadFile(path);
+            return decoder(encodedJson);
+        }
+
         public string ReadFile(string path)
         {
             if(path.Contains(".json"))
